881d74a8d4e8cb24692cba482bfc4a3fef2153eb
oluwaseyi47 <kayodeoluwaseyi70@gmail.com>
2021-08-02 09:23:12 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let n = 0
+// Calculate the dot_product 
+
+function dotProduct(v1, v2){
+let product = 0;
+ 
+for (let i = 0; i < n; i++){
+    product += v1[i] * v2[i]
+}
+
+    return product
+}
+
+
+// finding the the orthogonal vectors.
